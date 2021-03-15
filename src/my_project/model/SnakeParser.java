23d376510f37a2a284a6d5c094b71bfb3e9abf8e
Rene785 <rene.greff@arcor.de>
2021-03-15 09:29:06 +0100

    SnakeScanner überarbeitet SnakeParser angefangen aber noch nicht fertig

--- src/my_project/model/SnakeParser.java
+++ src/my_project/model/SnakeParser.java
@@ -2,8 +2,21 @@ package my_project.model;
 
 public class SnakeParser implements Parser{
 
+    private SnakeScanner snakeScanner;
+    public SnakeParser(){ snakeScanner = new SnakeScanner();}
     @Override
     public boolean parse(String input) {
+        if(snakeScanner.scan(input)){
+            if(snakeScanner.getType().equals("START")){
+                if(snakeScanner.getType().equals("BEFEHL")){
+                    snakeScanner.nextToken();
+                    if(snakeScanner.getType().equals("KLAMMERAUF")){
+                        snakeScanner.nextToken();
+                    }
+                }
+            }
+
+        }
         return false;
     }
 
--- src/my_project/model/SnakeScanner.java
+++ src/my_project/model/SnakeScanner.java
@@ -12,46 +12,51 @@ public class SnakeScanner extends Scanner<String,String>{
         for(int i = 0; i<input.length();i++){
             if(input.charAt(i) == 'l'){
                 if(i<input.length()-4){
-                    Token tempT = new Token(input.substring(i,i+5),"RICHTUNG");
+                    Token tempT = new Token(input.substring(i,i+5),"BEFEHL");
                     if(tempT.equals("links")){
                         this.tokenList.append(tempT);
-                    }
-                }
-            }
-            if(input.charAt(i) == 'r'){
+                    }else return false;
+                }else return false;
+            }else if(input.charAt(i) == 'r'){
                 if(i<input.length()-5){
-                    Token tmpT = new Token(input.substring(i,i+6),"RICHTUNG");
+                    Token tmpT = new Token(input.substring(i,i+6),"BEFEHL");
                     if(tmpT.equals("rechts")){
                         this.tokenList.append(tmpT);
-                    }
-                }
-            }
-            if(input.charAt(i) == 'v'){
+                    }else return false;
+                }else return false;
+            }else if(input.charAt(i) == 'v'){
                 if(i<input.length()-2){
-                    Token temp = new Token(input.substring(i,i+4),"POSITION");
+                    Token temp = new Token(input.substring(i,i+4),"BEFEHL");
                     if(temp.equals("vor")){
                         this.tokenList.append(temp);
-                    }
-                }
-            }
-            if(input.charAt(i) == 'z'){
+                    }else return false;
+                }else return false;
+            }else if(input.charAt(i) == 'z'){
                 if(i<input.length()-6){
-                    Token temp = new Token(input.substring(i,i+7),"POSITION");
+                    Token temp = new Token(input.substring(i,i+7),"BEFEHL");
                     if(temp.equals("zurueck")){
                         this.tokenList.append(temp);
-                    }
-                }
-            }
-            if(input.charAt(i) == 'f'){
+                    }else return false;
+                }else return false;
+            }else if(input.charAt(i) == 'f'){
                 if(i<input.length()-6){
-                    Token temp = new Token(input.substring(i,i+7),"FRESSEN");
+                    Token temp = new Token(input.substring(i,i+7),"BEFEHL");
                     if(temp.equals("fressen")){
                         this.tokenList.append(temp);
-                    }
-                }
-            }
-
+                    }else return false;
+                }else return false;
+            }else if(input.charAt(i) == '('){
+                this.tokenList.append(new Token(input.charAt(i),"KLAMMERAUF"));
+            }else if(input.charAt(i) == ')'){
+                this.tokenList.append(new Token(input.charAt(i),"KLAMMERZU"));
+            }else if(input.charAt(i) == '{'){
+                this.tokenList.append(new Token(input.charAt(i),"START"));
+            }else if(input.charAt(i) == '}'){
+                this.tokenList.append(new Token(input.charAt(i),"ENDE"));
+            }else return false;
         }
-        return false;
+        this.tokenList.append(new Token("#","NODATA"));
+        tokenList.toFirst(); // WICHTIG!
+        return true;
     }
 }
